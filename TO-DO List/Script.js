5d950fe48b7502923b6d256785604001d31bea91
gaurav7393 <130067797+gaurav7393@users.noreply.github.com>
2024-04-25 12:26:13 +0530

    Add files via upload

---
+++ TO-DO List/Script.js
@@ -0,0 +1,34 @@
+const inputbox=document.getElementById("input-box")
+const list=document.getElementById("list")
+ function add(){
+    if(inputbox.value===""){
+        alert(`add some task !`);
+    }
+    else{
+        let li=document.createElement("li")
+        li.innerHTML=inputbox.value;
+        list.appendChild(li)
+        let span=document.createElement("span")
+        span.innerHTML="\u00d7"
+        li.appendChild(span)
+    }
+    inputbox.value="";
+savedata();
+ }
+ list.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked")
+        savedata()
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove()
+savedata()
+    }
+ },false)
+ function savedata(){
+    localStorage.setItem("data",list.innerHTML)
+ } 
+ function showdata(){
+    list.innerHTML= localStorage.getItem("data")
+ } 
+ showdata()
